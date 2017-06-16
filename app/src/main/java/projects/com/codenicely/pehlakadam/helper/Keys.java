c5b962d509758d90489a97d1ddb715761feadfce
ujjwalagrawal17 <ujjwal.iitism@gmail.com>
2017-06-16 17:40:08 +0530

    Helper Module Added

---
+++ app/src/main/java/projects/com/codenicely/pehlakadam/helper/Keys.java
@@ -0,0 +1,12 @@
+package projects.com.codenicely.pehlakadam.helper;
+
+/**
+ * Created by Meghal on 6/19/2016.
+ */
+public class Keys {
+
+    public static final String KEY_IMAGE_LIST = "image_list";
+    public static final String KEY_POSITION_IMAGE = "position";
+    public static String KEY_CATEGORY_ID="category_id";
+
+}
---
+++ app/src/main/java/projects/com/codenicely/pehlakadam/helper/image_loader/ImageLoader.java
@@ -0,0 +1,16 @@
+package projects.com.codenicely.pehlakadam.helper.image_loader;
+
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+/**
+ * Created by meghal on 13/10/16.
+ */
+
+public interface ImageLoader {
+
+    void loadImage(String url, ImageView imageView, ProgressBar progressBar);
+
+    void loadImage1(String image_url, ImageView aah_imageView);
+//    void load_circular_image(String url, ImageView imageView);
+}
